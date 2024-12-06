4f4c0e38d6ca6372b38a3856611ef725a8976883
JorgeRight34 <jorgelright34rd30012003@gmail.com>
2024-12-06 13:45:16 -0400

    Now is app

--- backend/wasteCollector/wsgi.py
+++ backend/wasteCollector/wsgi.py
@@ -13,7 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wasteCollector.settings')
 
-if os.environ.get('PROD') == 'True':
-    app = get_wsgi_application()
-else: 
-    application = get_wsgi_application()
+
+app = get_wsgi_application()
